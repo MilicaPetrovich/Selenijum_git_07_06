c687157e04c3856908786619122ab496f802cf5b
rapunzel12 <126329140+rapunzel12@users.noreply.github.com>
2024-06-07 19:25:10 +0200

    Novi komit

--- src/test/java/Pages/SidebarPage.java
+++ src/test/java/Pages/SidebarPage.java
@@ -1,2 +1,29 @@
-package Pages;public class SidebarPage {
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SidebarPage {
+    // trazenje lokatora
+
+    WebDriver driver;
+
+    WebElement practiceButton;
+
+    public SidebarPage(WebDriver driver){
+        this.driver=driver;
+    }
+
+    public WebElement getPracticeButton() {
+        return driver.findElement(By.id("menu-item-20"));
+    }
+
+    //-----------------------------------
+    // akcije nad lokatorima
+
+    public void clickOnPracticeButton(){
+        getPracticeButton().click();
+    }
+
 }
--- src/test/java/Tests/LoginTests.java
+++ src/test/java/Tests/LoginTests.java
@@ -1,2 +1,28 @@
-package Tests;public class LoginTests {
-}
+package Tests;
+
+import Base.BaseTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTests extends BaseTest{
+
+        @BeforeMethod
+        public void pageSetUp(){
+            driver.manage().window().maximize();
+            driver.get("https://practicetestautomation.com/");
+
+        }
+
+        @Test
+        public void userCanLogIn(){
+            sidebarPage.clickOnPracticeButton();
+            practicePage.clickOnGoToLoginPage();
+        }
+
+
+
+
+
+    }
+
+
